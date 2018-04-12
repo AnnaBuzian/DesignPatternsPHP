fab067c83b962712f1efe8356bb298a4e78421f9
AnnaBuzian <buzyan2008@gmail.com>
2018-04-12 03:03:11 +0300

    virtual proxy - image

--- Structural/Proxy/virtual_proxy.php
+++ Structural/Proxy/virtual_proxy.php
@@ -56,17 +56,6 @@ class ImageVirtualProxy implements ImageInterface
     }
 
     /**
-     * @param $size
-     * @return string
-     */
-    public static function convert($size)
-    {
-        $unit=array('b','kb','mb','gb','tb','pb');
-        $size = $size/pow(1024,($i=floor(log($size,1024))));
-        return @round($size, 2).' '.$unit[$i];
-    }
-
-    /**
      * Инициализация
      */
     private function init() {
@@ -76,16 +65,33 @@ class ImageVirtualProxy implements ImageInterface
     }
 }
 
-$img1 = new ImageVirtualProxy(__DIR__ .'/images/image1.jpg');
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
-$img2 = new ImageVirtualProxy(__DIR__ .'/images/image2.jpg');
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
-$img3 = new ImageVirtualProxy(__DIR__ .'/images/image3.jpg');
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
+class Memory
+{
+    /**
+     * @param $size
+     * @return string
+     */
+    public static function getUsageConvert()
+    {
+        $size = memory_get_usage();
+        $unit=array('b','kb','mb','gb','tb','pb');
+        $size = $size/pow(1024,($i=floor(log($size,1024))));
+        return @round($size, 2).' '.$unit[$i];
+    }
+}
 
+$img1 = new ImageVirtualProxy(__DIR__ .'/images/image1.jpg');
+echo 'Image1: ', Memory::getUsageConvert(), "<br>";
+$img2 = new ImageVirtualProxy(__DIR__ .'/images/image2.jpg');
+echo 'Image2: ', Memory::getUsageConvert(), "<br>";
+$img3 = new ImageVirtualProxy(__DIR__ .'/images/image3.jpg');
+echo 'Image3: ', Memory::getUsageConvert(), "<br>";
+echo "----------------------------------------------<br><br>";
+
+// Инициализация объекта класса Image
 $size1 = $img1->getSize();
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
+echo 'Image1: ', Memory::getUsageConvert(), "<br>";
 $size2 = $img2->getSize();
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
+echo 'Image2: ', Memory::getUsageConvert(), "<br>";
 $size3 = $img3->getSize();
-var_dump(ImageVirtualProxy::convert(memory_get_usage()));
+echo 'Image3: ', Memory::getUsageConvert(), "<br>";
