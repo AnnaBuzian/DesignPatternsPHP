069e23d673e4a67b4f124b17d31d99442f5e8b49
Бритик Анна Николаевна <anna.buzyan@privatbank.ua>
2018-05-29 09:54:14 +0300

    fixed memento test

--- Behavioral/Memento/Tests/MementoTest.php
+++ Behavioral/Memento/Tests/MementoTest.php
@@ -8,21 +8,6 @@ use PHPUnit\Framework\TestCase;
 
 class MementoTest extends TestCase
 {
-    public function testSaveStatePerson()
-    {
-        $person = new Person(
-            'Петров',
-            'Петр',
-            '+380660000000',
-            'ул. Набережная Победы 30, г. Днепр'
-        );
-
-        $caretaker = new PersonCaretaker();
-        $caretaker->setMemento($person->saveMemento());
-
-        $this->assertEquals("Петров", (string) $person->getFirstName());
-    }
-
     public function testRestoreStatePerson()
     {
         $person = new Person(
@@ -36,8 +21,9 @@ class MementoTest extends TestCase
         $caretaker->setMemento($person->saveMemento());
 
         $person->setFirstName("Сидоров");
-        $person->restoreMemento($caretaker->getMemento());
+        $this->assertEquals("Петров", (string) $person->getFirstName());
 
+        $person->restoreMemento($caretaker->getMemento());
         $this->assertEquals("Петров", (string) $person->getFirstName());
     }
 }
